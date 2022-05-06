5348489271ca29bdfff0ddb6fc726fad87b8c9d1
Vinod Lala <vinod_lala@usa.net>
2022-05-06 10:47:39 -0400

    Refactor

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import { SliderData } from './SliderData';
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
 
 const ImageSlider = ({ slides }) => {
@@ -22,7 +21,7 @@ const ImageSlider = ({ slides }) => {
     <section className="slider">
       <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
       <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
-      {SliderData.map((slide, index) => {
+      {slides.map((slide, index) => {
         return(
           <div className={index === current ? 'slide active': 'slide'} key={index}>
             {index === current && (
