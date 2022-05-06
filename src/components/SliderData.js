c585778ea22288b7309a6b79458cc2f17bfaf9c3
Vinod Lala <vinod_lala@usa.net>
2022-05-06 13:47:10 -0400

    Add and re-order images

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -4,23 +4,51 @@ export const SliderData = [
     image: 'https://images.unsplash.com/photo-1548094967-e25a127d1f6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1523&q=80'
   },
   {
-    name: 'handbag',
-    image: 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80'
-  },
-  {
-    name: 'shoes',
-    image: 'https://images.unsplash.com/photo-1618274158630-bc47a614b3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    name: 'filipino food',
+    image: 'https://images.unsplash.com/photo-1625477811233-044633d10dd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
   },
   {
     name: 'indian food',
     image: 'https://images.unsplash.com/photo-1585937421612-70a008356fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
   },
   {
-    name: 'ipad',
-    image: 'https://images.unsplash.com/photo-1612529377791-b3de2b201f0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3648&q=80'
+    name: 'gucci',
+    image: 'https://images.unsplash.com/photo-1548036328-c9fa89d128fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3VjY2l8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
+  },
+  {
+    name: 'handbag',
+    image: 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80'
   },
   {
     name: 'car',
     image: 'https://thedrive.com/content/2021/09/2022_NX-450h_Redline_067_ChrisTsui_021.jpg?quality=85'
+  },
+  {
+    name: 'church',
+    image: 'https://images.unsplash.com/photo-1438032005730-c779502df39b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80'
+  },
+  {
+    name: 'bless',
+    image: 'https://images.unsplash.com/photo-1591635025117-875881dee88c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80'
+  },
+  {
+    name: 'ipad',
+    image: 'https://images.unsplash.com/photo-1612529377791-b3de2b201f0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3648&q=80'
+  },
+  {
+    name: 'black shoes',
+    image: 'https://images.unsplash.com/photo-1618274158630-bc47a614b3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+  },
+  {
+    name: 'iphone',
+    image: 'https://images.unsplash.com/photo-1530319067432-f2a729c03db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1989&q=80'
+  },
+  {
+    name: 'beige shoes',
+    image: 'https://images.unsplash.com/photo-1535043934128-cf0b28d52f95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+  },
+  {
+    name: "happy mother's day",
+    image: 'https://www.viber.com/app/uploads/happy-mom.jpg'
   }
 ]
