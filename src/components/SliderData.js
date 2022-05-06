5410c0ac5a2e8c16c401584056f67b755a53f563
Vinod Lala <vinod_lala@usa.net>
2022-05-06 15:16:29 -0400

    Fix typo

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -3,7 +3,7 @@ import {HiOutlineHeart} from 'react-icons/hi';
 export const SliderData = [
   {
     name: 'roses',
-    copy: 'Rose are red',
+    copy: 'Roses are red',
     image: 'https://images.unsplash.com/photo-1548094967-e25a127d1f6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1523&q=80'
   },
   {
