ebf654cb8a2dab43b694fd68b93105ed5c2636b9
Vinod Lala <vinod_lala@usa.net>
2022-05-06 10:59:26 -0400

    Add names to images, add shoes image

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,11 +1,18 @@
 export const SliderData = [
   {
+    name: 'roses',
     image: 'https://images.unsplash.com/photo-1548094967-e25a127d1f6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1523&q=80'
   },
   {
+    name: 'handbag',
     image: 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80'
   },
   {
+    name: 'ipad',
     image: 'https://images.unsplash.com/photo-1612529377791-b3de2b201f0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3648&q=80'
+  },
+  {
+    name: 'shoes',
+    image: 'https://images.unsplash.com/photo-1618274158630-bc47a614b3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
   }
 ]
