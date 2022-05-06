7333f36e8ea88145897711716371dc476cc50472
Vinod Lala <vinod_lala@usa.net>
2022-05-06 10:46:18 -0400

    Clean up code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import ImageSlider from "./components/ImageSlider";
 import { SliderData } from './components/SliderData';
 
-
 function App() {
   return <ImageSlider slides={SliderData} />;
 }
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -14,26 +14,24 @@ const ImageSlider = ({ slides }) => {
     setCurrent(current === 0 ? length - 1 : current - 1)
   }
 
-  console.log(current);
-
   if(!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
 
   return (
-      <section className="slider">
-        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
-        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
-        {SliderData.map((slide, index) => {
-          return(
-            <div className={index === current ? 'slide active': 'slide'} key={index}>
-              {index === current && (
-                <img src={slide.image} alt='travel image' className="image" />
-              )}
-            </div>
-          )
-        })}
-      </section>
+    <section className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
+      {SliderData.map((slide, index) => {
+        return(
+          <div className={index === current ? 'slide active': 'slide'} key={index}>
+            {index === current && (
+              <img src={slide.image} alt='travel image' className="image" />
+            )}
+          </div>
+        )
+      })}
+    </section>
   );
 }
 
