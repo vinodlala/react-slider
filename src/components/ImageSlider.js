1780b2fb1d3aa45a0d0abb13b7a74c50c99bdf5e
Vinod Lala <vinod_lala@usa.net>
2022-05-06 15:11:05 -0400

    Add copy including hearts

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -25,7 +25,10 @@ const ImageSlider = ({ slides }) => {
         return(
           <div className={index === current ? 'slide active': 'slide'} key={index}>
             {index === current && (
-              <img src={slide.image} alt='travel image' className="image" />
+              <>
+                <img src={slide.image} alt='travel image' className="image" />
+                <div className="text">{slide.copy}</div>
+              </>
             )}
           </div>
         )
--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,54 +1,69 @@
+import {HiOutlineHeart} from 'react-icons/hi';
+
 export const SliderData = [
   {
     name: 'roses',
+    copy: 'Rose are red',
     image: 'https://images.unsplash.com/photo-1548094967-e25a127d1f6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1523&q=80'
   },
   {
     name: 'filipino food',
+    copy: 'Lechon is juicy',
     image: 'https://images.unsplash.com/photo-1625477811233-044633d10dd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
   },
   {
     name: 'indian food',
+    copy: 'White rice is white',
     image: 'https://images.unsplash.com/photo-1585937421612-70a008356fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
   },
   {
     name: 'gucci',
+    copy: 'This handbag is Gucci',
     image: 'https://images.unsplash.com/photo-1548036328-c9fa89d128fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3VjY2l8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
   },
   {
     name: 'handbag',
+    copy: 'THIS handbag is RED',
     image: 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80'
   },
   {
     name: 'car',
+    copy: "Just like Mom's Lexus",
     image: 'https://thedrive.com/content/2021/09/2022_NX-450h_Redline_067_ChrisTsui_021.jpg?quality=85'
   },
   {
     name: 'church',
+    copy: 'She drives it to church',
     image: 'https://images.unsplash.com/photo-1438032005730-c779502df39b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80'
   },
   {
     name: 'bless',
+    copy: 'To get God to bless us',
     image: 'https://images.unsplash.com/photo-1591635025117-875881dee88c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80'
   },
   {
     name: 'ipad',
+    copy: 'Mom loves her iPad',
     image: 'https://images.unsplash.com/photo-1612529377791-b3de2b201f0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3648&q=80'
   },
   {
     name: 'black shoes',
+    copy: 'She also loves shoes',
     image: 'https://images.unsplash.com/photo-1618274158630-bc47a614b3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
   },
   {
     name: 'iphone',
+    copy: 'And texts on her iPhone',
     image: 'https://images.unsplash.com/photo-1530319067432-f2a729c03db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1989&q=80'
   },
   {
     name: 'beige shoes',
+    copy: 'She also loves shoes',
     image: 'https://images.unsplash.com/photo-1535043934128-cf0b28d52f95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
   },
   {
     name: "happy mother's day",
+    copy: <><HiOutlineHeart /> <HiOutlineHeart /> <HiOutlineHeart /></>,
     image: 'https://www.viber.com/app/uploads/happy-mom.jpg'
   }
 ]
