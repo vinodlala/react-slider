df631147a8ec92d86dc5b3818a28f268ad8639bf
Vinod Lala <vinod_lala@usa.net>
2022-05-06 10:07:10 -0400

    Update css, handle no data

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -6,6 +6,10 @@ const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0)
   const length = slides.length
 
+  if(!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
       <section className="slider">
         {/*<FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />*/}
@@ -13,7 +17,7 @@ const ImageSlider = ({ slides }) => {
         <FaArrowAltCircleLeft className="left-arrow" />
         <FaArrowAltCircleRight className="right-arrow" />
         {SliderData.map((slide, index) => {
-          return <img src={slide.image} alt='travel image' />;
+          return <img src={slide.image} alt='travel image' className="image" />;
         })}
       </section>
   );
