e3d694c02564653daed06bdfe105cd83abf707bf
Vinod Lala <vinod_lala@usa.net>
2022-05-06 10:42:50 -0400

    Go to next/prev slide

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -6,18 +6,32 @@ const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0)
   const length = slides.length
 
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  console.log(current);
+
   if(!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
 
   return (
       <section className="slider">
-        {/*<FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />*/}
-        {/*<FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />*/}
-        <FaArrowAltCircleLeft className="left-arrow" />
-        <FaArrowAltCircleRight className="right-arrow" />
+        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
         {SliderData.map((slide, index) => {
-          return <img src={slide.image} alt='travel image' className="image" />;
+          return(
+            <div className={index === current ? 'slide active': 'slide'} key={index}>
+              {index === current && (
+                <img src={slide.image} alt='travel image' className="image" />
+              )}
+            </div>
+          )
         })}
       </section>
   );
