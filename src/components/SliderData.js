08945e596cea05cdd56a00576e62a3cf152048f9
Vinod Lala <vinod_lala@usa.net>
2022-05-06 11:13:11 -0400

    Add and reorder images

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -8,11 +8,19 @@ export const SliderData = [
     image: 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80'
   },
   {
+    name: 'shoes',
+    image: 'https://images.unsplash.com/photo-1618274158630-bc47a614b3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+  },
+  {
+    name: 'indian food',
+    image: 'https://images.unsplash.com/photo-1585937421612-70a008356fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+  },
+  {
     name: 'ipad',
     image: 'https://images.unsplash.com/photo-1612529377791-b3de2b201f0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3648&q=80'
   },
   {
-    name: 'shoes',
-    image: 'https://images.unsplash.com/photo-1618274158630-bc47a614b3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    name: 'car',
+    image: 'https://thedrive.com/content/2021/09/2022_NX-450h_Redline_067_ChrisTsui_021.jpg?quality=85'
   }
 ]
